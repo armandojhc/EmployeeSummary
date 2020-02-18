ffc4142eece91cd4a77c266fa63bea69af8fbdd0
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-02-18 00:42:26 -0500

    Fixed html generating issues, finally managed to generate html

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 // const util = require("util");
 
-const manager = require("./manager");
+const manager = require("./lib/manager");
+const intern = require("./lib/intern");
+const engineer = require("./lib/engineer");
 
 // const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -13,6 +15,12 @@ var teamMembers = {
   engineers: [],
   interns: []
 }
+
+// var teamMembers = {
+//   manager: new manager(23, 45, "Armando", "Lead Dev", "rdd@rtetr.com"),  //There can be only one
+//   engineers: [new engineer("rdear4", 234, "Ron", "rdef@kent.edu"), new engineer("armandojhc", 12, "Armando 2", "Other Dev", "dfg@lejr.com")],
+//   interns: [new intern("John", "GW", 87, , "Intern 1", "asdfgh@lko.com"), new intern("MIT", 11, "David", "Intern 2", "34567dfgh@lko.com")]
+// }
 
 function promptUser() {
     inquirer.prompt([
@@ -37,22 +45,14 @@ function promptUser() {
       case "Add an engineer":
         console.log("Adding an engineer...");
           //Call the addEngineer function
-          if (teamMembers.engineer === null) {
+         
             addEngineer();
-          } else {
-            console.log("Your team already has an engineer. You cannot add another");
-            promptUser();
-          }
+          
         break;
       case "Add an intern":
           console.log("Adding an intern...");
           //Call the add intern function
-          if (teamMembers.intern === null) {
             addIntern();
-          } else {
-            console.log("Your team already has an intern. You cannot add another");
-            promptUser();
-          }
         break;
 
 // function promptUser() {
@@ -95,6 +95,16 @@ function promptUser() {
       console.log("Building Team profile...");
       //Use the teamMembers Object to build the team roster HTML file
       //Then quit.
+      let teamProfileHTML = createHTML();
+
+      fs.writeFile("teamProfile.html", teamProfileHTML, function(err) {
+        if (err) {
+          console.log("There was an error writing you html to a file");
+          console.log(err);
+        } else {
+          console.log("Write successful");
+        }
+      });
         
       console.log("Quitting...");
       break;
@@ -121,11 +131,6 @@ function addManager() {
     },
     {
       type: "input",
-      message: "What is the manager's title?",
-      name: "managerTitle"
-    },
-    {
-      type: "input",
       message: "What is the manager's email?",
       name: "managerEmail"
     },
@@ -137,9 +142,10 @@ function addManager() {
   ]).then(response => {
     // console.log(response);
     //Create a manager insance using new Mananger
-    let aManager = new manager(response.managerOfficeNumber, response.managerId, response.managerName, response.managerTitle, response.managerEmail);
-    console.log(aManager);
+    let aManager = new manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber);
+    //console.log(aManager);
     teamMembers.manager = aManager;
+    console.log(teamMembers);
     promptUser();
   })
 }
@@ -159,25 +165,21 @@ function addEngineer() {
     },
     {
       type: "input",
-      message: "What is the engineer's title?",
-      name: "engineerTitle"
-    },
-    {
-      type: "input",
       message: "What is the engineer's email?",
       name: "engineerEmail"
     },
     {
       type: "input",
       message: "What is the engineer's github?",
-      name: "gihub"
+      name: "github"
     }
   ]).then(response => {
     // console.log(response);
     //Create a manager insance using new Mananger
-    let aEngineer = new engineer(response.engineerName, response.engineerId, response.engineerTitle, response.gihub, response.engineerEmail);
-    console.log(aEngineer);
-    teamMembers.engineer = aEngineer;
+    let aEngineer = new engineer(response.engineerName, response.engineerId, response.engineerEmail, response.github);
+    
+    teamMembers.engineers.push(aEngineer);
+    
     promptUser();
   })
 }
@@ -197,11 +199,6 @@ function addIntern() {
     },
     {
       type: "input",
-      message: "What is the intern's title?",
-      name: "internTitle"
-    },
-    {
-      type: "input",
       message: "What is the inter's email?",
       name: "internEmail"
     },
@@ -213,11 +210,77 @@ function addIntern() {
   ]).then(response => {
     // console.log(response);
     //Create a manager insance using new Mananger
-    let aIntern = new intern(response.internName, response.internId, response.internTitle, response.internEmail, response.internSchool);
-    console.log(aIntern);
-    teamMembers.intern = aIntern;
+    let aIntern = new intern(response.internName, response.internId, response.internEmail, response.internSchool);
+    
+    teamMembers.interns.push(aIntern);
+    
     promptUser();
   })
+}
+
+function createHTML() {
+
+  //Create the engineers string
+  let engineers = "";
+  let interns = "";
+  // console.log(teamMembers.engineers[0].generateHTML());
+  for (let anEngineer of teamMembers.engineers) {
+    let htmlString = anEngineer.generateHTML();
+
+    engineers += htmlString;
+  }
+
+  //Create the interns string
+  for (let anIntern of teamMembers.interns) {
+    let htmlString = anIntern.generateHTML();
+
+    interns += htmlString;
+  }
+
+  let teamHTMLTemplateString = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>manager</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <div class="jumbotron">
+        <h1 style="text-align: center;"> My Team</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="card" style="width: 18rem; float: left;">
+          <div class="card-body">
+            <h5 class="card-title">Name: ${teamMembers.manager.name}</h5>
+            <img src="https://img.icons8.com/ios-filled/48/000000/cafe.png">
+            <h6 class="card-text">Manager</h6>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${teamMembers.manager.id}</li>
+            <li class="list-group-item">Email: ${teamMembers.manager.email}</li>
+            <li class="list-group-item">Office number: ${teamMembers.manager.officeNumber}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row" style="margin-top:20px">
+        ${engineers}
+      </div>
+      <div class="row" style="margin-top:20px">
+        ${interns}
+      </div>
+        
+    
+    </div>
+    <a href="https://icons8.com/icon/37302/cafe" style="position: relative; margin-left: 500px;">Icons8</a>
+    
+</body>
+</html>`
+
+return teamHTMLTemplateString;
+
 }
 
 // function Answers (name , id , email, office) {
